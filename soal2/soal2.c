5319d9805b855cefa736dc7be1797e25bac4db36
axelbrians <axelbrians@gmail.com>
2021-04-16 23:04:14 +0700

    fix banget ini mah

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -21,13 +21,13 @@ typedef struct stack_t {
 void stack_init(Stack *stack);
 void stack_push(Stack *stack, char value[], char type[], char fullName[], char old[]);
 void stack_for_each(Stack *stack, int option);
-void stack_push_dir(Stack *stack, char value[]); 
+void stack_push_dir(Stack *stack, char value[]);
+void stack_push_duplicate(Stack *stack, char buffer[]);
 
 // problem fucntion declaration
 void move_file();
 void modified_mkdir();
 void create_dir(char name[]);
-
 
 
 int main() {
@@ -41,7 +41,7 @@ int main() {
     }
 
     if (pid_1 == 0) {
-        printf("masuk ke child dulu, ini unzip\n");
+        // printf("masuk ke child dulu, ini unzip\n");
 
         char *argv[] = { "unzip", "pets.zip", "*.jpg", "-d", "./petshop", NULL };
 
@@ -49,7 +49,7 @@ int main() {
     } else {
         while(wait(&status) > 0);
 
-        printf("unzip selesai, balik ke parent\n");
+        // printf("unzip selesai, balik ke parent\n");
         modified_mkdir();
         
     }
@@ -60,10 +60,15 @@ int main() {
 void modified_mkdir() {
     pid_t child;
     char *buffer;
-    char type[100], fullName[100], value[100], temp[100], old[20];
+    char type[100], fullName[100], value[100], temp[100], old[20], 
+    type2[100], value2[100], temp2[100], old2[100];
 
     Stack myStack;
     stack_init(&myStack);
+
+    Stack duplicateFile;
+    stack_init(&duplicateFile);
+
     struct dirent *de;
     DIR *dr = opendir("./petshop");
 
@@ -82,18 +87,48 @@ void modified_mkdir() {
         strcpy(value, buffer);
         buffer = strtok(NULL, ";");
         strcpy(temp, buffer);
-
-        for (int i = 0; i < 10; i++) {
-            if (temp[i] == '.' || temp[i] == NULL || temp[i] == '_'){
+        // printf("before: %s\n", temp);
+        // todo cp buat file yg isinya dobel
+        for (int i = 0; i < 20; i++) {
+            if (temp[i] == '_') {
                 old[i] = '\0';
+                int j = 2, k = 0;
+                while (1) {
+                    type2[k] = temp[j];
+                    j++; k++;
+                    if (temp[j] == '\0') break;
+                }
+                type2[k] = '\0';
+                k = 0;
+
+                buffer = strtok(NULL, ";");
+                strcpy(value2, buffer);
+                buffer = strtok(NULL, "g");
+                strcpy(old2, buffer);
+                while(1) {
+                    if (old2[k] == 'j') {
+                        old2[k - 1] = '\0';
+                        break;
+                    }
+                    k++;
+                }
+                // printf("%s %s %s\n", type2, value2, old2);
+                stack_push(&duplicateFile, value2, type2, fullName, old2);
                 break;
             }
 
+            if (temp[i] == 'j'){
+                old[i - 1] = '\0';
+                break;
+            }
+        
             old[i] = temp[i];
         }
-        printf("%s \n", old);
+        // printf("%s \n", old);
         // printf("nama file: %s type: %s fullName: %s\n", buffer, type, fullName);
         stack_push(&myStack, value, type, fullName, old);
+
+
     }
 
     child = fork();
@@ -111,7 +146,7 @@ void modified_mkdir() {
         }
 
         // nge scan nama buat bikin dir
-        printf("nge scan nama file buat bikin dir\n");
+        // printf("nge scan nama file buat bikin dir\n");
         while ((de = readdir(dr)) != NULL){
             if (de->d_name[0] == '.') continue;
             buffer = strtok(de->d_name, ";");
@@ -119,13 +154,13 @@ void modified_mkdir() {
         }
 
         closedir(dr);
-        printf("initiate bikin dir\n");
+        // printf("initiate bikin dir\n");
         stack_for_each(&dirStack, 1);
         exit(EXIT_SUCCESS);
     } else {
         while(wait(NULL) > 0);
 
-
+        stack_for_each(&duplicateFile, 2);
         stack_for_each(&myStack, 3);
 
         exit(EXIT_SUCCESS);
@@ -154,7 +189,7 @@ void create_dir(char name[]) {
     }
 }
 
-void move_file(char value[], char type[], char fullName[], char old[]) {
+void move_file(char value[], char type[], char fullName[], char old[], int option) {
     pid_t child;
 
 
@@ -185,10 +220,14 @@ void move_file(char value[], char type[], char fullName[], char old[]) {
     }
 
     if (child == 0) {
-    
-        char *argv[] = { "mv", origin, newPath, NULL };
-        execv("/bin/mv", argv);
-        // exit(EXIT_SUCCESS);
+        if (option == 1) {
+            char *argv[] = { "mv", origin, newPath, NULL };
+            execv("/bin/mv", argv);
+            // exit(EXIT_SUCCESS);
+        } else {
+            char *argv[] = { "cp", origin, newPath, NULL };
+            execv("/bin/cp", argv);
+        }
     } else {
         while (wait(NULL) > 0);
         
@@ -199,16 +238,21 @@ void move_file(char value[], char type[], char fullName[], char old[]) {
 
 void stack_for_each(Stack *stack, int option) {
     StackNode *temp = stack->top;
-    printf("==mulai for each==\n");
+    // printf("==mulai for each==\n");
     if (option == 1) {
         while (temp) {
-            printf("nama dir: %s\n", temp->key);
+            // printf("nama dir: %s\n", temp->key);
             create_dir(temp->key);
+            temp = temp->next;
+        }
+    } else if (option == 2) {
+        while (temp) {
+            move_file(temp->key, temp->type, temp->fullName, temp->old, 2);
             temp = temp->next;
         }
     } else if (option == 3) {
         while (temp) {
-            move_file(temp->key, temp->type, temp->fullName, temp->old);
+            move_file(temp->key, temp->type, temp->fullName, temp->old, 1);
             temp = temp->next;
         }
     }
